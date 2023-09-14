4e3831a83f616c802538828ae6cd7b29e00d3ba0
Khaled Ramadan <khaledramdan880@gmail.com>
2023-09-14 11:54:28 +0300

    Splash Screen

---
+++ lib/features/auth/presentation/screens/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:stars_movie/core/routes/routes.dart';
+import 'package:stars_movie/core/utils/app_images.dart';
+import 'package:stars_movie/core/utils/commons.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+         navigateReplacement(context, Routes.login);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(AppImages.logo),
+      ),
+    );
+  }
+}
